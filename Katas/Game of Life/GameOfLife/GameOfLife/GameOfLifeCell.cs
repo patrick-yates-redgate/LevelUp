e24f3eb8cfccc3b8811e0b1f18b66af90a2d0ca8
patrick-yates-redgate <patrick.yates@red-gate.com>
2023-02-17 18:12:43 +0000

    Now passes all tests, count rows and cols together

--- Katas/Game of Life/GameOfLife/GameOfLife/GameOfLifeCell.cs
+++ Katas/Game of Life/GameOfLife/GameOfLife/GameOfLifeCell.cs
@@ -4,7 +4,7 @@ public class GameOfLifeCell
 {
     private GameOfLife Board { get; }
     private (int x, int y) Pos { get; }
-    
+
     public GameOfLifeCell(GameOfLife board, int x, int y)
     {
         Board = board;
@@ -14,14 +14,12 @@ public class GameOfLifeCell
     public int CountNeighbours()
     {
         var count = 0;
-        for (var x = -1; x <= 1; x += 2)
+        for (var x = -1; x <= 1; ++x)
         {
-            count += Board.SafeValue(Pos.x + x, Pos.y) == true ? 1 : 0;
-        }
-        
-        for (var y = -1; y <= 1; y += 2)
-        {
-            count += Board.SafeValue(Pos.x, Pos.y + y) == true ? 1 : 0;
+            for (var y = -1; y <= 1; ++y)
+            {
+                count += (x != 0 || y != 0) && Board.SafeValue(Pos.x + x, Pos.y + y) == true ? 1 : 0;
+            }
         }
 
         return count;
