975b85798a0d55c96fe111e3590500c2c58539ad
Patrick Yates <patrick.yates@red-gate.com>
2024-12-20 16:40:48 +0000

    [CodingGame2024] Style updates

--- CodinGame Winter Challenge 2024/CodinGameWinter2024/CodinGameWinter2024/Program.cs
+++ CodinGame Winter Challenge 2024/CodinGameWinter2024/CodinGameWinter2024/Program.cs
@@ -13,40 +13,40 @@ class Player
     static void Main(string[] args)
     {
         string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
-        int width = int.Parse(inputs[0]); // columns in the game grid
-        int height = int.Parse(inputs[1]); // rows in the game grid
+        inputs = Console.ReadLine()!.Split(' ');
+        var width = int.Parse(inputs[0]); // columns in the game grid
+        var height = int.Parse(inputs[1]); // rows in the game grid
 
         // game loop
         while (true)
         {
-            int entityCount = int.Parse(Console.ReadLine());
+            var entityCount = int.Parse(Console.ReadLine()!);
             Console.Error.WriteLine($"EntityCount: {entityCount}");
 
-            for (int i = 0; i < entityCount; i++)
+            for (var i = 0; i < entityCount; i++)
             {
-                inputs = Console.ReadLine().Split(' ');
-                int x = int.Parse(inputs[0]);
-                int y = int.Parse(inputs[1]); // grid coordinate
-                string type = inputs[2]; // WALL, ROOT, BASIC, TENTACLE, HARVESTER, SPORER, A, B, C, D
-                int owner = int.Parse(inputs[3]); // 1 if your organ, 0 if enemy organ, -1 if neither
-                int organId = int.Parse(inputs[4]); // id of this entity if it's an organ, 0 otherwise
-                string organDir = inputs[5]; // N,E,S,W or X if not an organ
-                int organParentId = int.Parse(inputs[6]);
-                int organRootId = int.Parse(inputs[7]);
+                inputs = Console.ReadLine()!.Split(' ');
+                var x = int.Parse(inputs[0]);
+                var y = int.Parse(inputs[1]); // grid coordinate
+                var type = inputs[2]; // WALL, ROOT, BASIC, TENTACLE, HARVESTER, SPORER, A, B, C, D
+                var owner = int.Parse(inputs[3]); // 1 if your organ, 0 if enemy organ, -1 if neither
+                var organId = int.Parse(inputs[4]); // id of this entity if it's an organ, 0 otherwise
+                var organDir = inputs[5]; // N,E,S,W or X if not an organ
+                var organParentId = int.Parse(inputs[6]);
+                var organRootId = int.Parse(inputs[7]);
             }
-            inputs = Console.ReadLine().Split(' ');
-            int myA = int.Parse(inputs[0]);
-            int myB = int.Parse(inputs[1]);
-            int myC = int.Parse(inputs[2]);
-            int myD = int.Parse(inputs[3]); // your protein stock
-            inputs = Console.ReadLine().Split(' ');
-            int oppA = int.Parse(inputs[0]);
-            int oppB = int.Parse(inputs[1]);
-            int oppC = int.Parse(inputs[2]);
-            int oppD = int.Parse(inputs[3]); // opponent's protein stock
-            int requiredActionsCount = int.Parse(Console.ReadLine()); // your number of organisms, output an action for each one in any order
-            for (int i = 0; i < requiredActionsCount; i++)
+            inputs = Console.ReadLine()!.Split(' ');
+            var myA = int.Parse(inputs[0]);
+            var myB = int.Parse(inputs[1]);
+            var myC = int.Parse(inputs[2]);
+            var myD = int.Parse(inputs[3]); // your protein stock
+            inputs = Console.ReadLine()!.Split(' ');
+            var oppA = int.Parse(inputs[0]);
+            var oppB = int.Parse(inputs[1]);
+            var oppC = int.Parse(inputs[2]);
+            var oppD = int.Parse(inputs[3]); // opponent's protein stock
+            var requiredActionsCount = int.Parse(Console.ReadLine()!); // your number of organisms, output an action for each one in any order
+            for (var i = 0; i < requiredActionsCount; i++)
             {
 
                 // Write an action using Console.WriteLine()
